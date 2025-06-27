c409f6839e3a4f13adc5df228d41f971621bec6b
Claudevspace <claudia.tracanaa@gmail.com>
2025-06-27 16:57:45 -0300

    carga imagenes convenios

---
+++ src/components/areaEconomica.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function areaEconomica(){
+    return(
+        <div className="externalContent">
+            <iframe
+            src= "https://ensiladores.com.ar/InfoSocios/SeccionEcoPrincipal.php"
+            width= "100%"
+            className="external-content"
+            />
+
+        </div>
+
+    );
+}
